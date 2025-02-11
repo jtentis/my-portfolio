fdd2acfc087017ad3f7e17fe0b53d933278029f9
João Tentis <101594270+jtentis@users.noreply.github.com>
2025-02-10 22:30:28 -0300

    Feat: ajeitando links

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,9 @@
 export function Footer() {
     return (
         <footer className="bg-primary text-secondary dark:bg-secondary dark:text-primary flex justify-center gap-10">
-            <p>Code</p>
-            <p>Made by João Tentis</p>
-            <p>Contacts</p>
+            <a href="https://github.com/jtentis/my-portfolio" target="_blank" className="footerHover">Code</a>
+            <a href="https://www.instagram.com/tnszpj" target="_blank" className="footerHover">Made by João Tentis</a>
+            <a href="mailto:jps.tentis@gmail.com" className="footerHover">Contato</a>
         </footer>
     );
 }
