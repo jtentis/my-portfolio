ac89a95ae1a9cfb4ccd0860ed15321589c32059f
João Tentis <101594270+jtentis@users.noreply.github.com>
2025-02-20 20:31:58 -0300

    Feat: adicionando react-scan

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="pt" className="w-full h-full">
             <head>
+                {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" /> */}
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <Meta />
