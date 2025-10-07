679aa2a1f42f2ba6669cc103f085325e8a6e2f81
João Tentis <101594270+jtentis@users.noreply.github.com>
2025-10-07 00:18:18 -0300

    Style: ajeitando titulos

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -15,8 +15,16 @@ const AboutMe = () => {
     }, [t.about.title, t.about.description]);
 
     return (
-        <div className="flex items-center justify-center">
-            <h1>{t.about.title}</h1>
+        <div className="flex flex-col justify-between p-7">
+            <div className="mb-5">
+                <div className="flex gap-2">
+                    <div className="h-2.5 w-2.5 mt-1 rounded-full border border-foreground bg-card"></div>
+                    <div className="h-2.5 w-2.5 mt-1 rounded-full border border-foreground bg-card"></div>
+                    <div className="h-2.5 w-2.5 mt-1 rounded-full border border-foreground bg-card"></div>
+                    <h1 className="font-bold">{t.about.title}</h1>
+                    <div className="w-[90%] border-b-2 self-center border-dashed border-secondary/20 dark:border-primary/20" />
+                </div>
+            </div>
         </div>
     );
 };
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,15 +6,25 @@ export default function Home() {
 
     useEffect(() => {
         document.title = t.home.title;
-        const descriptionMeta = document.querySelector('meta[name="description"]');
+        const descriptionMeta = document.querySelector(
+            'meta[name="description"]'
+        );
         if (descriptionMeta) {
             descriptionMeta.setAttribute("content", t.home.description);
         }
     }, [t.home.title, t.home.description]);
 
     return (
-        <div className="flex flex-col items-center justify-center">
-            <h1>{t.home.title}</h1>
+        <div className="flex flex-col justify-between p-7">
+            <div className="mb-5">
+                <div className="flex gap-2">
+                    <div className="h-2.5 w-2.5 mt-1 rounded-full border border-foreground bg-card"></div>
+                    <div className="h-2.5 w-2.5 mt-1 rounded-full border border-foreground bg-card"></div>
+                    <div className="h-2.5 w-2.5 mt-1 rounded-full border border-foreground bg-card"></div>
+                    <h1 className="font-bold">{t.home.title}</h1>
+                    <div className="w-full border-b-2 self-center border-dashed border-secondary/20 dark:border-primary/20" />
+                </div>
+            </div>
         </div>
     );
 }
