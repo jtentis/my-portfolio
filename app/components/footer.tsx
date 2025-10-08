8151209b4a399bde2017275bfffe7431e3570880
João Tentis <101594270+jtentis@users.noreply.github.com>
2025-10-07 23:32:57 -0300

    Fix: atualizando strings

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import { useLanguage } from "~/hooks/useLanguage";
+
 export function Footer() {
+    const { t } = useLanguage();
+    
+        useEffect(() => {
+            document.title = t.projects.title;
+            const descriptionMeta = document.querySelector(
+                'meta[name="description"]'
+            );
+            if (descriptionMeta) {
+                descriptionMeta.setAttribute("content", t.projects.description);
+            }
+        }, [t.projects.title, t.projects.description]);
+
     return (
         <footer className=" text-secondary dark:text-primary flex justify-center gap-10">
-            <a href="https://github.com/jtentis/my-portfolio" target="_blank" className="footerHover">Code</a>
-            <a href="https://www.instagram.com/tnszpj" target="_blank" className="footerHover">Made by João Tentis</a>
-            <a href="mailto:jps.tentis@gmail.com" className="footerHover">Contato</a>
+            <a href="https://github.com/jtentis/my-portfolio" target="_blank" className="footerHover">{t.footer.code}</a>
+            <a href="https://www.instagram.com/tnszpj" target="_blank" className="footerHover">{t.footer.madeBy}</a>
+            <a href="mailto:jps.tentis@gmail.com" className="footerHover">{t.footer.contact}</a>
         </footer>
     );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,7 +7,6 @@ import { useLanguage } from "../hooks/useLanguage";
 
 export default function Home() {
     const { t } = useLanguage();
-    console.log(typeof t.home.description)
 
     useEffect(() => {
         document.title = t.home.title;
@@ -23,15 +22,15 @@ export default function Home() {
         <div className="flex flex-col">
             <Title title={t.nav.home} />
             <div className="flex flex-col justify-center items-start h-[450px] gap-10">
-                <p className="list__item-col text-3xl">
+                <span className="list__item-col text-3xl">
                     <TextAnimator text="$ cat welcome.txt" blinkOnEnd={true}/>
-                </p>
-                <p className="font-bold text-7xl">
+                </span>
+                <span className="font-bold text-7xl">
                     <TextAnimator text="João Tentis" />
-                </p>
-                <p className="text-3xl">
+                </span>
+                <span className="text-3xl">
                     <TextAnimator text={t.home.description} />
-                </p>
+                </span>
                 <Link to="/projects" className={`navLinksHome`}>
                     {t.home.button}<IoMdArrowForward className="ml-3" size={20}/>
                 </Link>
