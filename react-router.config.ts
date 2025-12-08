37ab3e8152793813717d0529e0b7a2f6da1e62cd
João Tentis <101594270+jtentis@users.noreply.github.com>
2025-12-08 00:38:26 -0300

    cloudfare test sla qual

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,9 @@ import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false, 
+  future: {
+    unstable_viteEnvironmentApi: true,
+  },
+  presets: [vercelPreset()],  
 } satisfies Config;
